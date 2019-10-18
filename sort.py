eaed90b869f213c367838ffce596c0da5584c4a7
Jeng-Yeu Shih <shihjengyeu@gmail.com>
2019-10-19 00:27:41 +0800

    Add SelectionSort.

--- sort.py
+++ sort.py
@@ -1,11 +1,14 @@
 class Sort:
+    def _swap(self, collection, i, j):
+        temp = collection[i]
+        collection[i] = collection[j]
+        collection[j] = temp
+
     def BubbleSort(self, collection):
         for i in range(len(collection), 0, -1):
             for j in range(i - 1):
                 if collection[j] > collection[j + 1]:
-                    temp = collection[j]
-                    collection[j] = collection[j + 1]
-                    collection[j + 1] = temp
+                    self._swap(collection, j, j + 1)
 
     def InsertionSort(self, collection):
         for i in range(len(collection)):
@@ -14,3 +17,9 @@ class Sort:
                     temp = collection[j]
                     collection[j] = collection[i]
                     collection[i] = temp
+
+    def SelectionSort(self, collection):
+        for i in range(len(collection)):
+            for j in range(i, len(collection)):
+                if collection[j] < collection[i]:
+                    self._swap(collection, i, j)
--- sort_test.py
+++ sort_test.py
@@ -5,7 +5,10 @@ arr = [9, 0, 2, 5, 7, 1, 3, 4, 8, 6, 10]
 mySort.BubbleSort(arr)
 print('Bubble Sort:   ', arr)
 
-mySort = sort.Sort()
 arr = [9, 0, 2, 5, 7, 1, 3, 4, 8, 6, 10]
 mySort.InsertionSort(arr)
+print('Insertion Sort:', arr)
+
+arr = [9, 0, 2, 5, 7, 1, 3, 4, 8, 6, 10]
+mySort.SelectionSort(arr)
 print('Insertion Sort:', arr)
