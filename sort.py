d22561004bd7014292afd0bcaa2ff0726e703adb
Jeng-Yeu Shih <shihjengyeu@gmail.com>
2019-10-21 19:57:27 +0800

    Modify InsertionSort.

--- sort.py
+++ sort.py
@@ -14,9 +14,7 @@ class Sort:
         for i in range(len(collection)):
             for j in range(i):
                 if collection[i] < collection[j]:
-                    temp = collection[j]
-                    collection[j] = collection[i]
-                    collection[i] = temp
+                    self._swap(collection, i, j)
 
     def SelectionSort(self, collection):
         for i in range(len(collection)):
